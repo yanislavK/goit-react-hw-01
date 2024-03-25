2684b50f3830521f10bef239ca79ec0c40b0d2e2
yanislavK <121428054+yanislavK@users.noreply.github.com>
2024-03-25 20:32:36 +0100

    profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,26 +3,24 @@ import "./Profile.css";
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
     <div className="profile">
-      {" "}
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>{tag}</p>
-        <p>{location}</p>{" "}
-      </div>{" "}
-      <ul>
-        {" "}
-        <li>
+      <div className="description">
+        <img className="avatar" src={image} alt="User avatar" />
+        <p className="name">{name}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+      <ul className="profile-list">
+        <li className="profile-list-item">
           <span>Followers</span>
-          <span>{stats.followers}</span>{" "}
-        </li>{" "}
-        <li>
+          <span>{stats.followers}</span>
+        </li>
+        <li className="profile-list-2nd-item">
           <span>Views</span>
-          <span>{stats.views}</span>{" "}
-        </li>{" "}
-        <li>
+          <span>{stats.views}</span>
+        </li>
+        <li className="profile-list-item">
           <span>Likes</span> <span>{stats.likes}</span>{" "}
-        </li>{" "}
+        </li>
       </ul>
     </div>
   );
