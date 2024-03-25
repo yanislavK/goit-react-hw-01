750ee1cb22f369a167793b93d5a5d06f24091cb7
yanislavK <121428054+yanislavK@users.noreply.github.com>
2024-03-25 19:04:37 +0100

    base

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-irregular-whitespace */
+import Profile from "./Profile";
+import userData from "../userData.json";
+
+const App = () => {
+  return (
+    <>
+      {" "}
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />{" "}
+    </>
+  );
+};
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import "./Profile.css";
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className="profile">
+      {" "}
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>{tag}</p>
+        <p>{location}</p>{" "}
+      </div>{" "}
+      <ul>
+        {" "}
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>{" "}
+        </li>{" "}
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>{" "}
+        </li>{" "}
+        <li>
+          <span>Likes</span> <span>{stats.likes}</span>{" "}
+        </li>{" "}
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
